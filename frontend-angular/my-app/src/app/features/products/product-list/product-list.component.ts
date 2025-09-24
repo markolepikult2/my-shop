d3336ee3c06762a5d6e291a902d712b97c97d100
marko.lepikult <marko.lepikult@gmail.com>
2025-09-24 13:14:05 +0300

    Pagination added to product list.

--- frontend-angular/my-app/src/app/features/products/product-list/product-list.component.ts
+++ frontend-angular/my-app/src/app/features/products/product-list/product-list.component.ts
@@ -1,14 +1,20 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { ProductComponent } from '../product/product.component';
 import { ProductListModel } from '../models/product-list.model';
 import { RouterLink } from '@angular/router';
+import { PaginationComponent } from '../../../shared/pagination/pagination.component';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, ProductComponent, RouterLink],
+  imports: [
+    CommonModule,
+    ProductComponent,
+    RouterLink,
+    PaginationComponent
+  ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
@@ -19,15 +25,20 @@ export class ProductListComponent {
   error?: string;
 
   ngOnInit() {
-    // Try to load from API gateway; fall back to mock on error.
-    this.http.get<ProductListModel>('http://localhost:8080/products').subscribe({
+    this.fetchProducts(0);
+  }
+
+  fetchProducts(page: number): void {
+    this.loading = true;
+    this.error = undefined;
+    this.http.get<ProductListModel>(`http://localhost:8080/products?page=${page}`).subscribe({
       next: (data) => {
-          console.debug("Products loaded:", data);
-          this.productList = data ?? null; this.loading = false;
-          console.debug("Products loaded 2:", this.productList); },
+        this.productList = data;
+        this.loading = false;
+      },
       error: () => {
         this.loading = false;
-        this.error = 'Showing demo data (API unreachable)';
+        this.error = 'API is unreachable';
         console.error("Failed to load products from API gateway");
       }
     });
